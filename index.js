d62769d01b8a80fae8dc27a254e31bae6126fd93
Tade77 <adekunletaofeeq@mail.com>
2023-03-12 01:26:04 +0100

    WIP: Javascript functionality

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const countries = [
-  "Albania",
-  "Bolivia",
-  "Canada",
-  "Denmark",
-  "Ethiopia",
-  " Finland",
-];
-console.log(mapCon);
+const slide = document.getElementById("slide");
+const up = document.getElementById("up");
+const down = document.getElementById("down");
+
+let y = 0;
+
+up.addEventListener("click", (e) => {
+  if (y > "-900") {
+    y = y - 300;
+    slide.style.top = y + "px";
+  }
+});
+down.addEventListener("click", (e) => {
+  if (y < 0) {
+    y = y + 300;
+    down.style.top = y + "px ";
+  }
+});
